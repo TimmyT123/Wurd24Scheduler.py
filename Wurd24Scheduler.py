f37ef9861f30950920c8d80822ca5b84f00c0cc1
TimmyT123 <timmyt123@hotmail.com>
2023-12-15 23:38:52 -0700

    - Added BYE to single teams - Added @everybody before every WEEK

--- Wurd24Scheduler.py
+++ Wurd24Scheduler.py
@@ -1,7 +1,6 @@
 import re
 
 
-
 pathSched = './wurd24sched.csv'
 pathUsers = './wurd24users.csv'
 pathNFL_Teams = './NFL_Teams.csv'
@@ -24,14 +23,21 @@ user_bye = []
 def space_between_uu_and_uc_games(games_text):
     first = False
     games_lst = games_text.split('\n')
-    pattern = r'\w*\((\w)\) vs \w*\((\w)'
+
     for i, game in enumerate(games_lst):
         if 'week'.lower() in game.lower():
             first = False
-        if len(game) < 10:
+
+        if '(' not in game:  # this makes sure game has a user or cpu in it
             continue
 
         try:
+            pattern2 = r'^\w*\(U\)$'
+            m2 = re.match(pattern2, game)
+            if m2:
+                games_lst[i] += ' - BYE'  # Add BYE to single teams
+
+            pattern = r'\w*\((\w)\) vs \w*\((\w)'
             m = re.match(pattern, game).groups()
             if m[0] != m[1] and not first:
                 games_lst.insert(i, '\n')
@@ -39,11 +45,32 @@ def space_between_uu_and_uc_games(games_text):
         except:
             continue
 
-    games_lst = [game + '\n' for game in games_lst]
-    games_text = ''.join(games_lst)
-    print(games_text)
-    quit()
-    return games_text
+    # ADD @everybody to the front of each week
+    space = 0
+    games_lst_copy = games_lst.copy()
+    for i, word in enumerate(games_lst_copy):
+        if 'week'.lower() in word.lower():
+            space += 1
+            games_lst.insert(i+space-1, '@everybody')
+
+    games_lst = [game + '\n' for game in games_lst]  # put return in at the end of games
+
+    games_text = ''.join(games_lst) # make into a string
+
+    new_games_text = ''
+    num = 0
+    for char in games_text:
+        if char == '\n':  # get rid of triple \n
+            num += 1
+            if num == 3:
+                num = 0
+                continue
+            new_games_text += char
+        else:
+            num = 0
+            new_games_text += char
+
+    return new_games_text
 
 
 def comp_or_user(games):
@@ -53,13 +80,13 @@ def comp_or_user(games):
             team_txt = team_txt + str(team.strip('\n') + '(U)\n')
         else:
             team_txt = team_txt + str(team.strip('\n') + '(C)\n')
-        #games.replace(team.strip('\n'), team_txt)
-    #test_team = [team.replace(team.strip('\n'), team_txt + '\n') for team in games]
+
     games_txt = ''.join(games)
     team_lst = team_txt.split()
     for team_r in team_lst:
         games_txt = games_txt.replace(team_r[0:-3], team_r)
     return games_txt
+
 
 def check_if_user_not_in_current_week_games(weekgames):
     if len(weekgames) < 2:
@@ -83,7 +110,6 @@ def check_if_user_not_in_current_week_games(weekgames):
     gameDupTempList2.clear()
 
 
-
 for game in sched:
     if 'week' in game.lower():
         check_if_user_not_in_current_week_games(gamesTemp)
@@ -95,16 +121,10 @@ for game in sched:
     for user in users:
         user1 = user.strip('\n')
         if user1 in game:
-            #game = game.replace(user1, user1 + '(U)')
             gamesTemp.append(game.replace(',', ' vs '))
 
 check_if_user_not_in_current_week_games(gamesTemp)
 games_txt = comp_or_user(gameDupTempList)
 games_txt = space_between_uu_and_uc_games(games_txt)
 print(games_txt)
-# for userGame in gamesTemp:
-#     print(userGame, end='')
-    # if game[0] == ',':
-    #     for game in usersTemp:
-    #         print(game)
-          #usersTemp = [line for line in open(pathUsers)]
+
